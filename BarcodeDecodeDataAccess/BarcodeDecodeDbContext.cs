5bfe8cf91d82e061658fef8be98a80978260b1e0
DoctorWater <malckov.schura2015@gmail.com>
2025-03-22 15:21:11 +0300

    feat: DataAccess project added

---
+++ BarcodeDecodeDataAccess/BarcodeDecodeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BarcodeDecodeLib.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarcodeDecodeDataAccess;
+
+public class BarcodeDecodeDbContext : DbContext
+{
+    DbSet<TransportStorageUnit> TransportStorageUnits { get; set; }
+    DbSet<TransportOrder> TransportOrders { get; set; }
+    DbSet<LocationTicket> LocationTickets { get; set; }
+        
+    public BarcodeDecodeDbContext() { }
+    public BarcodeDecodeDbContext(DbContextOptions<BarcodeDecodeDbContext> options) : base(options) { }
+}
