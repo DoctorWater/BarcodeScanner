4f52d8530ea9055be17881f25b759c06cecfcd31
DoctorWater <malckov.schura2015@gmail.com>
2025-05-23 15:57:14 +0300

    chores: TransportOrderRelaunchMessage constructors changed

--- BarcodeDecodeLib/Models/Dtos/Messages/TransportOrder/TransportOrderRelaunchMessage.cs
+++ BarcodeDecodeLib/Models/Dtos/Messages/TransportOrder/TransportOrderRelaunchMessage.cs
@@ -4,18 +4,11 @@ namespace BarcodeDecodeLib.Models.Dtos.Messages.TransportOrder;
 
 public class TransportOrderRelaunchMessage : HttpMessage
 {
+    
     public TransportOrderRelaunchMessage(string barcode)
     {
         CorrelationId = Guid.NewGuid();
         Barcode = barcode;
-    }
-
-    [JsonConstructor]
-    public TransportOrderRelaunchMessage(Guid correlationId, string barcode, List<int> destinations)
-    {
-        CorrelationId = correlationId;
-        Barcode = barcode;
-        Destinations = destinations;
     }
 
     public string Barcode { get; }
